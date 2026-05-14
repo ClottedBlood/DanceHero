533697c9fcfd18fdb09ba5f58383f691a453d885
ricveldre <fernando.martinez@uteca.edu.mx>
2026-05-13 20:39:14 -0600

    Botones

---
+++ Assets/Scrips/SoundData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundData", menuName = "Scriptable Objects/SoundData")]
+public class SoundData : ScriptableObject
+{
+    public string animationName;
+
+    public string songName;
+}
